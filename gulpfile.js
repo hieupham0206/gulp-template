268b38de8a3ff7026b8c0bedc74564965debe2ca
Hieu <phamquanghieu0206@gmail.com>
2017-11-24 11:08:34 +0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -15,82 +15,85 @@ const gulp = require('gulp'),
   browserSync = require('browser-sync'),
   sourcemaps = require('gulp-sourcemaps')
 const paths = {
-    src: 'src/',
-    dist: 'dist/'
+  src: 'src/',
+  dist: 'dist/'
 }
 
 gulp.task('html', function () {
-    return gulp.src(`${paths.src}/*.html`).pipe(htmlmin({
-        removeComments: true,
-        collapseWhitespace: true
-    })).pipe(rename({extname: '.html'})).pipe(gulp.dest(`${paths.dist}`))
+  return gulp.src(`${paths.src}/*.html`).pipe(htmlmin({
+    removeComments: true,
+    collapseWhitespace: true
+  })).pipe(rename({extname: '.html'})).pipe(gulp.dest(`${paths.dist}`))
 })
 
 gulp.task('styles', function () {
-    return gulp.src(`${paths.src}/sass/**/[!_]*.scss`).
-      pipe(sass({precision: 4}).on('error', sass.logError)).
-      pipe(autoprefixer('last 2 version')).
-      pipe(gulp.dest(`${paths.dist}/css/`)).
-      pipe(shorthand()).
-      pipe(concat('bundle.min.css')).
-      pipe(sourcemaps.init()).
-      pipe(cleanCss({shorthandCompacting: false})).
-      pipe(sourcemaps.write()).
-      pipe(gulp.dest(`${paths.dist}/css/bundle`))
+  return gulp.src(`${paths.src}/sass/**/[!_]*.scss`).
+    pipe(sass({precision: 4}).on('error', sass.logError)).
+    pipe(autoprefixer('last 2 version')).
+    pipe(gulp.dest(`${paths.dist}/css/`)).
+    pipe(shorthand()).
+    pipe(concat('bundle.min.css')).
+    pipe(sourcemaps.init()).
+    pipe(cleanCss({shorthandCompacting: false})).
+    pipe(sourcemaps.write()).
+    pipe(gulp.dest(`${paths.dist}/css/bundle`))
 })
 
 gulp.task('uglify', function () {
-    return gulp.src(`${paths.src}/js/custom/**/*.js`).
-      pipe(babel()).
-      pipe(gulp.dest(`${paths.dist}/js/`))
+  return gulp.src(`${paths.src}/js/custom/**/*.js`).
+    pipe(babel()).
+    pipe(gulp.dest(`${paths.dist}/js/`))
 })
 
 gulp.task('scripts', ['uglify'], function () {
-    return gulp.src(`${paths.dist}/js/**/*.js`).
-      pipe(concat('bundle.js')).
-      pipe(insert.prepend('(function(window,document){')).
-      pipe(insert.append('}(this,this.document));')).
-      pipe(sourcemaps.init()).
-      pipe(uglify()).
-      pipe(sourcemaps.write()).
-      pipe(rename({suffix: '.min'})).
-      pipe(gulp.dest(`${paths.dist}/js/bundle`))
+  gulp.src(`${paths.src}/js/**/*.js`).pipe(gulp.dest(`${paths.dist}/js/`))
+  gulp.src(`${paths.dist}/js/**/*.js`).
+    pipe(concat('bundle.js')).
+    pipe(insert.prepend('(function(window,document){')).
+    pipe(insert.append('}(this,this.document));')).
+    pipe(sourcemaps.init()).
+    pipe(uglify()).
+    pipe(sourcemaps.write()).
+    pipe(rename({suffix: '.min'})).
+    pipe(gulp.dest(`${paths.dist}/js/bundle`))
+
+  return true;
 })
 
 gulp.task('images', function () {
-    // Optimize & move
-    gulp.src(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`)
-    // Only new stuff
-      .pipe(newer(`${paths.dist}/img/`))
-      // Optimize
-      .pipe(imagemin())
-      // Copy
-      .pipe(gulp.dest(`${paths.dist}/img/`))
+  // Optimize & move
+  gulp.src(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`)
+  // Only new stuff
+    .pipe(newer(`${paths.dist}/img/`))
+    // Optimize
+    .pipe(imagemin())
+    // Copy
+    .pipe(gulp.dest(`${paths.dist}/img/`))
 
-    // Make WebP versions or PNG & JPG
-    gulp.src(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`)
-    // Only new stuff
-      .pipe(newer(`${paths.dist}/img/webp`))
-      // WebP
-      .pipe(webp())
-      // Publish
-      .pipe(gulp.dest(`${paths.dist}/img/webp`))
+  // Make WebP versions or PNG & JPG
+  gulp.src(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`)
+  // Only new stuff
+    .pipe(newer(`${paths.dist}/img/webp`))
+    // WebP
+    .pipe(webp())
+    // Publish
+    .pipe(gulp.dest(`${paths.dist}/img/webp`))
 
-    return true
+  return true
 })
 
-gulp.task('browser-sync', ['styles', 'html', 'scripts', 'images'], function() {
-    browserSync.init([`${paths.dist}/css/*.css`, `${paths.dist}/js/*.js`, `${paths.dist}/*.html`], {
-        server: {
-            baseDir: "./dist/"
-        }
-    });
-});
+gulp.task('browser-sync', ['styles', 'html', 'scripts', 'images'], function () {
+  browserSync.init([`${paths.dist}/css/*.css`, `${paths.dist}/js/*.js`, `${paths.dist}/*.html`], {
+    server: {
+      baseDir: './dist/'
+    }
+  })
+})
 
 gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(`${paths.src}/sass/**/*.scss`, ['styles'])
-    gulp.watch(`${paths.src}/js/**/*.js`, ['scripts'])
-    gulp.watch(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`, ['images'])
-    gulp.watch(`${paths.src}/*.html`, ['html'])
-    gulp.watch(`${paths.src}/html/**/*.html`, ['html'])
+  gulp.watch(`${paths.src}/sass/**/*.scss`, ['styles'])
+  gulp.watch(`${paths.src}/js/**/*.js`, ['scripts'])
+  gulp.watch(`${paths.src}/img/**/*.{jpg,png,svg,gif,jpeg}`, ['images'])
+  gulp.watch(`${paths.src}/*.html`, ['html'])
+  gulp.watch(`${paths.src}/html/**/*.html`, ['html'])
 })
